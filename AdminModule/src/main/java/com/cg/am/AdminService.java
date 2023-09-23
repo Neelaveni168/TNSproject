502b354b3d68c11d35884a2607659fe682e51fff
NeelaveniR <NeelaveniR@DESKTOP-MOAHJPF>
2023-09-23 21:18:53 +0530

    commited

---
+++ AdminModule/src/main/java/com/cg/am/AdminService.java
@@ -0,0 +1,33 @@
+package com.cg.am;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+public class AdminService {
+
+	
+	@Autowired
+	private AdminRepository repo;
+	public List<Admin> listAll()
+	{
+	return repo.findAll();
+	}
+	public void save(Admin admin)
+	{
+	repo.save(admin);
+	}
+	public Admin get(Integer id)
+	{
+	return repo.findById(id).get();
+	}
+	public void delete(Integer id)
+	{
+	repo.deleteById(id);
+	}
+	}
